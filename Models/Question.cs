0a45b4a38093ff20eb2f8a7a515ef798c5d41d7f
vitual457 <chandan.keelara@gmail.com>
2021-03-10 16:05:51 +0530

    initial-commit

---
+++ Models/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuizApp.Models
+{
+    public class Question
+    {
+        public long ID { get; set; }
+        public string QuestionString { get; set; }
+        public string Type { get; set; }
+        public string Answer { get; set; }
+        public string OptionA { get; set; }
+        public string OptionB { get; set; }
+        public string OptionC { get; set; }
+        public string OptionD { get; set; }
+     
+        public string AddedBy { get; set; }
+    }
+}
---
+++ Models/Submittion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuizApp.Models
+{
+    public class Submittion
+    {
+        public long ID { get; set; }
+        public string SubmittedAnswer { get; set; }
+        public string Result { get; set; }
+        public string SubmittedBy { get; set; }
+        public DateTime SubmittedOn { get; set; }
+        
+    }
+}
